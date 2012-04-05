6fbb06358bab17f3a1d82621bb5a1fdbbc882a54
cgswap <baffolobill@yandex.ru>
2012-04-05 18:06:02 +0400

    fix some css bugs

--- cgpeople_me/urls.py
+++ cgpeople_me/urls.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-
+    url(r'^iewarning/$', direct_to_template, {'template': 'iewarning.html'}, name='ie-warning'),
     url (r'', include('generic.urls')),
     url(r'^twitter/', include('twitter_users.urls')),
     #url(r'^messages/', include('django_messages.urls')),
